821ad85725611e45489f15de555dd6dda76ed5fc
Victor Trintim <49760038+Trintim@users.noreply.github.com>
2022-05-30 07:50:52 -0300

    fix

--- quiz/components/Questao.tsx
+++ quiz/components/Questao.tsx
@@ -1,16 +1,41 @@
 import QuestaoModel from "../model/questao";
 import styles from "../styles/Questao.module.css"
+import Enunciado from "./Enunciado";
+import Resposta from "./Resposta";
 
+const letras = [
+  { valor: "A", cor: "#0162ea" },
+  { valor: "B", cor: "#ab00a0" },
+  { valor: "C", cor: "#8da600" },
+  { valor: "D", cor: "#28645c" },
+];
 interface QuestaoProps{
     valor: QuestaoModel
+    respostaFornecida: (indice: number) => void
 }
 
 export default function Questao(props: QuestaoProps){
     const questao = props.valor
 
+    function renderizarRespostas(){
+        return questao.respostas.map((resposta, i) =>{
+            return (
+              <Resposta
+                key={i}
+                valor={resposta}
+                indice={i}
+                letra={letras[i].valor}
+                corFundoLetra={letras[i].cor}
+                respostaFornecida={props.respostaFornecida}
+              />
+            );
+        })
+    }
+
     return (
         <div className={styles.questao}>
-            <h1>Questão</h1>
+            <Enunciado texto={questao.enunciado}/>
+            {renderizarRespostas()}
         </div>
     )
 }
--- quiz/pages/index.tsx
+++ quiz/pages/index.tsx
@@ -1,15 +1,33 @@
 
+import { useState } from 'react';
 import Questao from '../components/Questao'
 import QuestaoModel from '../model/questao'
 import RespostaModel from '../model/resposta'
 
+const questaoMock = new QuestaoModel(1, "Qual é a melhor cor?", [
+  RespostaModel.errada("verde"),
+  RespostaModel.certa("Azul"),
+  RespostaModel.errada("Preta"),
+  RespostaModel.errada("Vermelha"),
+]);
+
 
 export default function Home() {
-  const questaoTeste = new QuestaoModel(1, "Melhor cor?", [
-    RespostaModel.errada("verde"),
-    RespostaModel.certa("Azul"),
-    RespostaModel.errada("Preta"),
-    RespostaModel.errada("Vermelha"),
-  ]);
-  return <Questao valor={questaoTeste} />;
+  const [questao, setQuestao] = useState(questaoMock)
+
+  function respostaFornecida(indice: number){
+    setQuestao(questao.responderCom(indice))
+  }
+
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      height: '100vh',
+      alignItems: 'center'
+     }}>
+      <Questao valor={questao}
+              respostaFornecida={respostaFornecida}/>
+    </div>
+  );
 }
